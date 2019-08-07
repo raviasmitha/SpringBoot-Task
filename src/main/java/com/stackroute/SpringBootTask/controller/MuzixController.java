8abdbb495ed3602ec4ec59a1c31791c70d21cf2b
raviasmitha <51950242+raviasmitha@users.noreply.github.com>
2019-08-07 11:02:00 +0530

    changed return type to track

--- src/main/java/com/stackroute/SpringBootTask/controller/MuzixController.java
+++ src/main/java/com/stackroute/SpringBootTask/controller/MuzixController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RequestMapping("api/v1")
 public class MuzixController {
 
-    MuzixService muzixService;
+    private MuzixService muzixService;
 
     @Autowired
     public MuzixController(MuzixService muzixService){
@@ -21,33 +21,33 @@ public class MuzixController {
     }
 
 
-    @PostMapping("/muzix")
+    @PostMapping("/track")
 
-    public ResponseEntity<?> saveMusix(@RequestBody Muzix muzix){
+    public ResponseEntity<?> saveTrack(@RequestBody Muzix muzix){
         ResponseEntity responseEntity;
-        muzixService.saveMusix(muzix);
+        muzixService.saveTrack(muzix);
         responseEntity= new ResponseEntity("Successfully created", HttpStatus.CREATED);
         return responseEntity;
     }
 
     /* This method will retrive musix by using Query parameter */
 
-    @GetMapping("/muzix")
+    @GetMapping("/tracks")
 
-    public ResponseEntity<List<Muzix>> getMusixs() {
+    public ResponseEntity<List<Muzix>> getAllTracks() {
 
-        List<Muzix> musixes = muzixService.getMusix();
-        return new ResponseEntity<List<Muzix>>(musixes, HttpStatus.CREATED);
+        List<Muzix> musixes = muzixService.getAllTracks();
+        return new ResponseEntity<List<Muzix>>(musixes, HttpStatus.OK);
 
 
     }
 
-    @GetMapping("/muzix/{id}")
+    @GetMapping("/track/{id}")
 
-    public ResponseEntity<?> getById(@PathVariable int id)  {
+    public ResponseEntity<?> getTrackById(@PathVariable int id)  {
 
         Muzix muzix = null;
-        muzix = muzixService.getById(id);
+        muzix = muzixService.getTrackById(id);
 
         return new ResponseEntity<Muzix>(muzix, HttpStatus.OK);
     }
@@ -56,28 +56,27 @@ public class MuzixController {
      This method will delete data by id
      */
 
-    @DeleteMapping("/muzix/{id}")
-    public String deleteMuzix(@PathVariable int id) {
-        muzixService.deleteById(id);
-        return "Data deleted";
+    @DeleteMapping("/track/{id}")
+    public ResponseEntity<Muzix> deleteTrackById(@PathVariable int id) {
+        muzixService.deleteTrackById(id);
+        Muzix muzix = muzixService.getTrackById(id);
+        return new ResponseEntity<Muzix>(muzix,HttpStatus.OK);
     }
 
     /*
     This method will update data by id
      */
 
-    @PutMapping("/muzix/{id}")
-    public ResponseEntity<Muzix> updateMusix(@RequestBody Muzix muzix, @PathVariable int id) {
+    @PutMapping("/track/{id}")
+    public ResponseEntity<Muzix> updateTrackById(@RequestBody Muzix muzix, @PathVariable int id) {
 
-        if (muzixService.updateById(muzix, id)) {
-            return ResponseEntity.notFound().build();
-        }
+        muzixService.updateTrackById(muzix, id);
+        Muzix muzix1 = muzixService.getTrackById(id);
+        return new ResponseEntity<Muzix>(muzix, HttpStatus.OK);
 
-        return ResponseEntity.noContent().build();
     }
-
-    @GetMapping("/name/{name}")
-    public ResponseEntity<List<Muzix>> getByname(@PathVariable String name) {
+    @GetMapping("/track/{name}")
+    public ResponseEntity<List<Muzix>> getTrackByname(@PathVariable String name) {
         List<Muzix> musix = muzixService.getBYName(name);
         return new ResponseEntity<List<Muzix>>(musix, HttpStatus.OK);
     }
